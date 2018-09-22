1a41275aaacd24fa39c7ae285c3d44eccb8a6f5a
github username <github email address>
2018-09-22 16:42:38 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,9 @@ class Shoe
 BRANDS=[]
   def initialize(brand)
     @brand = brand
-    if brand =="unique"
-    BRANDS << brand
-  end
+    if !BRANDS.include?(@brand)
+    BRANDS << @brand
+    end
   end
 
 
